fe6fc4829b0edd69203c8fa56576e1ad73c05532
Max Bazarov <max@bazarov.ru>
2019-04-25 18:39:44 +0300

    Improved batch resizing

--- Exporter.sketchplugin/Contents/Sketch/scripts/resize.sh
+++ Exporter.sketchplugin/Contents/Sketch/scripts/resize.sh
@@ -5,29 +5,8 @@
 # Usage: > ./batch_resize.sh IMG_FOLDER 
 
 initial_folder="$1" # You can use "." to target the folder in which you are running the script for example
-resized_folder_name="previews"
-destination_folder=$initial_folder"/"$resized_folder_name"/"
-mkdir "$destination_folder"
+cd "$initial_folder"
 
-## rezise regular images
-all_images=$(find -E "$initial_folder" -maxdepth 1 -iname "*.png" ! -iname "*@2x.png")
-
-while read -r image_full_path; do
-    filename=$(basename "$image_full_path");
-    destination_full_path=$destination_folder$filename;
-    
-    sips -Z 300 "$image_full_path" --out "$destination_full_path";
-
-done <<< "$all_images"
-
-
-## rezise retina images
-all_images=$(find -E "$initial_folder" -maxdepth 1 -iname "*@2x.png")
-
-while read -r image_full_path; do
-    filename=$(basename "$image_full_path");
-    destination_full_path=$destination_folder$filename;
-    
-    sips -Z 600 "$image_full_path" --out "$destination_full_path";
-
-done <<< "$all_images"
+rm -rf previews
+mkdir previews
+sips -Z 300 *.png --out previews/
